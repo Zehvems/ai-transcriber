7c0d23eea421325a43a678641b2f7bb2c357f433
Zehvems <mbakeebiznes@gmail.com>
2025-12-22 23:25:56 +0100

    feat: improved app script to the todays set

--- api.js
+++ api.js
@@ -2,7 +2,12 @@ export function fakeFetch(formData) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (Math.random() < 0.3) reject(new Error("Błąd sieci."));
-      resolve({ text: "Mock Tranksrypcja" });
+      resolve({ ok: false, text: "Mock Tranksrypcja" });
     }, 700);
   });
+}
+export async function checkStatus() {
+  const res = await fetch("http://localhost:3000/status");
+  if (!res.ok) throw new Error("Błąd.");
+  return await res.json();
 }
--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 import { errStatus, fileInfo, setStatus, setTranscript } from "./ui.js";
-import { fakeFetch } from "./api.js";
+import { fakeFetch, checkStatus } from "./api.js";
 const form = document.getElementById("transcribe-form");
 const input = document.getElementById("input");
 const btn = document.getElementById("btn");
 let file = undefined;
 let result = undefined;
 setTranscript("Waiting for file...");
+try {
+  const data = await checkStatus();
+  setTranscript(JSON.stringify(data, null, 2));
+  setStatus("Backend Alive.");
+} catch (error) {
+  errStatus("Backend offline");
+}
 input.addEventListener("change", () => {
   if (input.files[0]) {
     file = input.files[0];
@@ -25,19 +32,20 @@ form.addEventListener("submit", async (e) => {
   const formData = new FormData();
   formData.append("file", file);
   console.log(formData.get("file"));
-  // TODO: integrate fakeFetch here
   setStatus("Processing...");
   try {
     result = await fakeFetch(formData);
+    if (result.ok !== true) {
+      setStatus("Error (backend res status)");
+    } else {
+      setTranscript(result.text);
+      setStatus("Gotowe (mock – brak prawdziwego backendu)");
+    }
   } catch {
     errStatus("Transcription failed – network error");
     setTranscript("");
-
-    btn.disabled = false;
     return;
+  } finally {
+    btn.disabled = false;
   }
-
-  setStatus("Gotowe (mock – brak prawdziwego backendu)");
-  setTranscript(result.text);
-  btn.disabled = false;
 });
